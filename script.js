4a50317d84eb811e30b1d87df81ddac5058e85d1
robinvluggen1-glitch <robinvluggen1@gmail.com>
2026-08-01 00:15:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("PuffLand est prêt !");
+let compteur = localStorage.getItem("panier") || 0;
+
+document.getElementById("compteur").textContent = compteur;
+
+const boutons = document.querySelectorAll("button");
+
+boutons.forEach((bouton) => {
+    if (bouton.textContent.includes("Ajouter au panier")) {
+        bouton.addEventListener("click", () => {
+            compteur++;
+            localStorage.setItem("panier", compteur);
+            document.getElementById("compteur").textContent = compteur;
+        });
+    }
+});
